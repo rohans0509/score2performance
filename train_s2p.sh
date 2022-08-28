e748ac87ed381cbbb90c87ad87c953d950d3db77
rohans0509 <59282751+rohans0509@users.noreply.github.com>
2022-08-28 18:43:43 +0530

    Add train files

---
+++ train_s2p.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -p t4v2
+#SBATCH --exclude=gpu068
+#SBATCH --gres=gpu:4
+#SBATCH -c 8
+#SBATCH --qos=high
+#SBATCH --mem=64G
+#SBATCH --job-name=expt
+#SBATCH --output=out.log
+#SBATCH --ntasks=1
+
+source ~/sourcefile
+source ~/score2perfenv/bin/activate
+date;hostname;pwd;whoami
+
+python main.py --mode=train --config.dataset.path="Store/asap-dataset" --config.dataset.mode="score2perf" --workdir=autoenc_end2end --config=configs.py:autoencoder --config.pt_enc_path=None
